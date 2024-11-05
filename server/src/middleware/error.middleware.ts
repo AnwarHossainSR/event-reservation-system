64e69ea1070f5681e7358b29236d890b4d6a237b
Anwar Hossain <mahedianwar@gmail.com>
2024-11-05 14:41:54 +0600

    feat: added command to package.json

--- server/src/middleware/error.middleware.ts
+++ server/src/middleware/error.middleware.ts
@@ -11,25 +11,6 @@ function errorMiddleware(
     const status = error.status || 500;
     let message = error.message || 'Something went wrong';
 
-    // Wrong Mongodb Id error
-    if (error.name === 'CastError') {
-        const match = /path "([^"]+)"/.exec(error.message);
-        const invalidPath = match ? match[1] : 'unknown field';
-        message = `Resource not found. Invalid: ${invalidPath}`;
-    }
-
-    // Mongoose duplicate key error (generic approach)
-    if (error.name === 'MongoError') {
-        const duplicateMessage =
-            /E11000 duplicate key error collection: .* index: (.+)/.exec(
-                error.message
-            );
-        if (duplicateMessage) {
-            const fieldName = duplicateMessage[1];
-            message = `Duplicate ${fieldName} Entered`;
-        }
-    }
-
     // Wrong JWT error
     if (error.name === 'JsonWebTokenError') {
         message = `Json Web Token is invalid, Try again `;
